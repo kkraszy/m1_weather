0bb15ba9b7bed5be3ea1fc05b806325fc86dc941
Karol Kraszy <kkraszy@gmail.com>
2019-01-13 21:25:51 +0100

    fix

--- app/code/community/Kkraszy/WeatherModule/controllers/Adminhtml/Kkraszy/Weathermodule/WeatherController.php
+++ app/code/community/Kkraszy/WeatherModule/controllers/Adminhtml/Kkraszy/Weathermodule/WeatherController.php
@@ -1,8 +1,5 @@
 <?php
 
-use Cmfcmf\OpenWeatherMap;
-use Cmfcmf\OpenWeatherMap\Exception as OWMException;
-
 class Kkraszy_WeatherModule_Adminhtml_Kkraszy_Weathermodule_WeatherController extends Mage_Adminhtml_Controller_Action
 {
 
@@ -13,41 +10,4 @@ class Kkraszy_WeatherModule_Adminhtml_Kkraszy_Weathermodule_WeatherController ex
         $this->renderLayout();
     }
 
-    public function saveAction()
-    {
-
-
-        $selectedIds = $this->getRequest()->getParam('weather');
-
-        $collection = Mage::getModel('weathermodule/weathermodule')->getCollection();
-
-        $model->setData('product_id', $id);
-        $model->save();
-
-        Mage::getSingleton('adminhtml/session')->setFormData(false);
-        $this->_redirect('*/*/');
-    }
-
-    public function deleteAction()
-    {
-        $selectedIds = $this->getRequest()->getParam('weather');
-        $count = 0;
-        foreach ($selectedIds as $key => $val) {
-            $model = Mage::getModel('weathermodule/weathermodule')->load($val);
-            if (empty($model->getData())) {
-                continue;
-            }
-            $model->delete();
-            $count++;
-        }
-
-        if (!empty($count)) {
-            Mage::getSingleton('adminhtml/session')->addSuccess($count . ' produkty/ów zostały poprawnie usuniete z modułu pogody');
-        } else {
-            Mage::getSingleton('adminhtml/session')->addError('Nic nie zostało usuniete z modułu pogody');
-        }
-
-        Mage::getSingleton('adminhtml/session')->setFormData(false);
-        $this->_redirect('*/*/');
-    }
 }
